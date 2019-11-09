77410e8ffcb223b60c511e9edd8505b2e994516d
成小新 <35800413+zhaoboy9692@users.noreply.github.com>
2019-11-09 14:02:41 +0800

    企查查sign拦截脚本
    企查查sign拦截脚本

---
+++ qcc_mt.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2019-11-09 11:44
+@Author  : Xincheng.Zhao
+@Desc    :
+@Email   : zhaoboy9692@163.com
+@File    : qcc_mt.py
+"""
+
+import mitmproxy.http
+from mitmproxy import ctx
+
+
+class Demo(object):
+    def __init__(self):
+        pass
+
+    def request(self, flow: mitmproxy.http.HTTPFlow):
+        # 过滤非企查查接口地址
+        if flow.request.host != "appv3.qichacha.net":
+            return
+        print(flow.request.query.get("sign"))
+        ctx.log.info(f"sign is: {flow.request.query.get('sign')}")
+
+
+addons = [
+    Demo()
+]
